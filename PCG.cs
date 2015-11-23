a16383fd14dcdecd999b25a1dceb4653991e721e
KVKuhr <kvkuhr@gmail.com>
2015-11-24 02:28:04 +0900

    Susse v2

--- PCG.cs
+++ PCG.cs
@@ -1,31 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PCG : MonoBehaviour {
+public class PCG : MonoBehaviour
+{
 
 	[SerializeField]
-	private GameObject wallprefab;
+	private GameObject
+		wallprefab;
 	[SerializeField]
-	private int matrixSize = 100;
+	private int
+		matrixSize = 100;
+	private float cooldown = 5;
 
-
-	void Start () {
-		GameObject[,] matrix = new GameObject[matrixSize,matrixSize];
+	void Start ()
+	{
+		GameObject[,] matrix = new GameObject[matrixSize, matrixSize];
 	
 		for (int m = 0; m<matrixSize; m++) {
 			for (int n = 0; n<matrixSize; n++) {
-				bool wall = Random.value>=0.7;
-				if(m==0 || m == matrixSize-1 || n==0 || n == matrixSize-1 )
+				bool wall = Random.value >= 0.7;
+				if (m == 0 || m == matrixSize - 1 || n == 0 || n == matrixSize - 1)
 					wall = true;
 
 
-				if(wall){
-				Instantiate (wallprefab,new Vector3(m*10,n*10,0),Quaternion.identity);	
+				if (wall) {
+					Instantiate (wallprefab, new Vector3 (m * 10, n * 10, 0), Quaternion.identity);	
 				}
 			
 			}
 		}
 	}
-	
 
+	void FixedUpdate ()
+	{
+		cooldown -= 0.02f;
+		HandleInput ();
+
+	}
+
+	void HandleInput ()
+	{
+		if (Input.GetAxis ("Jump") == 1)
+			if (cooldown < 0)
+				Application.LoadLevel (Application.loadedLevel);
+	}
 }
